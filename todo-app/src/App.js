467d33bd9a3535e886ebcc756f985b080ff562cd
Akshit Kumar Bansal <akbansal6204@gmail.com>
2026-05-28 17:40:46 +0530

    add initial todo-app project

---
+++ todo-app/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Todo</h1>
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="input-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title?" />
+          </div>
+          <div className="input-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="What's the task description?" />
+          </div>
+          <div className="input-input-item">
+            <button type="button" className="primaryBtn">Add</button>
+          </div>
+        </div>
+        <div className="btn-area">
+          <button>Todo</button>
+          <button>Completed</button>
+        </div>
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
